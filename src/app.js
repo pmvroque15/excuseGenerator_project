d0dd9a4a563e15fcfaa31120d81e7aa93ca043be
pmvroque15 <pmvroque@gmail.com>
2025-12-09 12:44:30 +0800

    Functionality initialized, will now edit index.html for it to render on the screen

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+  ];
+
+  function getRandomArrayIndex(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return randomIndex;
+}
+
+  let randomWho = who[getRandomArrayIndex(who)]
+
+  document.getElementById("excuse1").innerHTML = randomWho;
+
 };
